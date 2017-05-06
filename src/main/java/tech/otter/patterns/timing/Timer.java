a8be701d8a76a14c9dd8b40f28cc891f5e06835a
John Lynn <john@silferein.com>
2017-05-06 10:35:05 -0700

    Add gradle support, timer pattern.

---
+++ src/main/java/tech/otter/patterns/timing/Timer.java
@@ -0,0 +1,28 @@
+/**
+ * @author John Lynn <john@otter.tech>
+ * @date 5/6/17
+ */
+public class Timer {
+    private long interval;
+    private long current;
+    private Action action;
+
+    public Timer(long interval, Action action) {
+        this.interval = interval;
+        this.current = 0;
+        this.action = action;
+    }
+
+    public void update(float delta) {
+        this.current += delta;
+
+        while(this.current >= this.interval) {
+            this.current -= this.interval;
+            this.action.execute();
+        }
+    }
+
+    interface Action {
+        void execute();
+    }
+}
